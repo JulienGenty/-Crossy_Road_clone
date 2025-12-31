23d4db1d0bec4c3627e1adc3269ed41c55759841
Julien <julien.genty.contact@gmail.com>
2025-12-31 16:14:15 +0100

    feature(engine): add of a Player, of a Camera and of a Renderer method for the first scene

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+import * as THREE from "three";
+import { Renderer } from "./components/Renderer";
+import { Camera } from "./components/Camera";
+import { Player } from "./components/Player";
 import "./style.css";
+
+const scene = new THREE.Scene();
+scene.add(Player());
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight();
+directionalLight.position.set(-100, -100, 200);
+scene.add(directionalLight);
+
+const camera = Camera();
+scene.add(camera);
+
+const renderer = Renderer();
+renderer.render(scene, camera);
 
 console.log("Hello World");
