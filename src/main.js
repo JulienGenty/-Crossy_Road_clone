21bc628832b2ddf3eaa0467eefcf8bcd75a00422
Julien <julien.genty.contact@gmail.com>
2026-01-01 22:53:03 +0100

    feature(player): added of the features of deplacement of player

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import * as THREE from "three";
 import { Renderer } from "./components/Renderer";
 import { Camera } from "./components/Camera";
 import { DirectionalLight } from "./components/directionalLight.js";
-import { Player } from "./components/Player";
-import { map, initializeMap, addRows } from "./components/Map";
+import { player } from "./components/Player";
+import { map, initializeMap } from "./components/Map";
 import { animateVehicles } from "./animateVehicles.js";
+import { animatePLayer } from "./animatePlayer.js";
+import "./collectUserInput.js";
 import "./style.css";
 
 const scene = new THREE.Scene();
-scene.add(Player());
+scene.add(player);
 scene.add(map);
 
 const ambientLight = new THREE.AmbientLight();
@@ -24,7 +26,6 @@ initializeGame();
 
 function initializeGame() {
   initializeMap();
-  // addRows();
 }
 
 const renderer = Renderer();
@@ -32,6 +33,6 @@ renderer.setAnimationLoop(animate);
 
 function animate() {
   animateVehicles();
-
+  animatePLayer();
   renderer.render(scene, camera);
 }
